05d05b4707513ea28796041154a2e2b6df7d6d2e
Isaac Dearman <isaac.dearman@gmail.com>
2024-10-15 11:01:21 +0000

    add favicon and ease of use

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition");
 
 })
@@ -22,6 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // creates random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
