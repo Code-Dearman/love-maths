df261f34d9b798c22af445cd13b516367443a0c4
Isaac Dearman <isaac.dearman@gmail.com>
2024-10-15 10:25:06 +0000

    add correct and incorrect increment functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :)")
+        incrementScore();
     } else {
         alert(`Hmm... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -74,12 +76,21 @@ function claculateCorrectAnswer() {
 
 }
 
-function incrementScore() {
 
+/**
+ * Gets current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets current tall of incrorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
